1808f3dea536eb350f65b674a88fae3d5b94ec5e
Phantomical <phantom@lynches.ca>
2021-04-02 17:34:16 -0400

    Initial implementation

---
+++ src/detector.rs
@@ -0,0 +1,89 @@
+use git2::{Commit, DiffDelta, DiffHunk, Oid, Repository};
+use std::collections::{HashMap, HashSet};
+use crate::GitResult;
+
+pub fn commit_dependencies<'repo>(
+  repo: &'repo Repository,
+  commit: &Commit<'repo>,
+) -> GitResult<Vec<Oid>> {
+  let mut deps = vec![];
+
+  for parent in commit.parents() {
+    deps.append(&mut find_dependencies_with_parent(repo, &commit, &parent)?);
+  }
+
+  Ok(deps)
+}
+
+fn find_dependencies_with_parent<'repo>(
+  repo: &'repo Repository,
+  dependent: &Commit<'repo>,
+  parent: &Commit<'repo>,
+) -> GitResult<Vec<Oid>> {
+  let mut options = git2::DiffOptions::new();
+  options.include_unmodified(true);
+  options.indent_heuristic(true);
+  let mut diff = repo.diff_tree_to_tree(
+    Some(&parent.tree()?),
+    Some(&dependent.tree()?),
+    Some(&mut options),
+  )?;
+
+  let mut options = git2::DiffFindOptions::new();
+  options.all(true);
+  diff.find_similar(Some(&mut options))?;
+
+  let mut blames = HashMap::new();
+  let mut deps = HashSet::new();
+
+  let mut err = Ok(());
+  let mut cb = |delta: DiffDelta, hunk: DiffHunk| {
+    let mut inner = |delta: DiffDelta, hunk: DiffHunk| -> GitResult<()> {
+      let old_file = delta.old_file();
+      if old_file.id().is_zero() {
+        return Ok(());
+      }
+
+      let path = old_file.path().unwrap();
+      let blame = match blames.get(path) {
+        Some(blame) => blame,
+        None => {
+          let mut options = git2::BlameOptions::new();
+          options.track_copies_same_commit_copies(true);
+          options.track_copies_same_commit_moves(true);
+          options.track_copies_same_file(true);
+          let blame = repo.blame_file(path, Some(&mut options))?;
+          blames.entry(path.to_owned()).or_insert(blame)
+        }
+      };
+
+      for line in hunk.new_start()..hunk.new_start() + hunk.new_lines() {
+        let bhunk = match blame.get_line(line as _) {
+          Some(hunk) => hunk,
+          None => continue,
+        };
+
+        deps.insert(bhunk.final_commit_id());
+      }
+
+      Ok(())
+    };
+
+    match inner(delta, hunk) {
+      Ok(()) => true,
+      Err(e) => {
+        err = Err(e);
+        false
+      }
+    }
+  };
+  diff.foreach(
+    &mut |_, _| true,
+    None,
+    Some(&mut cb),
+    Some(&mut |_, _, _| true),
+  )?;
+  err?;
+
+  Ok(deps.into_iter().collect())
+}
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use clap::Arg;
+use git2::Repository;
+
+mod detector;
+
+type GitResult<T> = Result<T, git2::Error>;
+
+fn _main(commit_ids: Vec<&str>) -> GitResult<()> {
+  let repo = Repository::open(&std::env::current_dir().unwrap())?;
+
+  for id in commit_ids {
+    let rev = repo.revparse_single(id)?;
+    let commit = repo.find_commit(rev.id())?;
+    let deps = detector::commit_dependencies(&repo, &commit)?;
+
+    for dep in deps {
+      println!("{}", dep);
+    }
+  }
+
+  Ok(())
+}
+
+fn main() {
+  let matches = clap::App::new("git-deps")
+    .about("Auto-detects commits on which the given commit(s) depend")
+    .usage("git deps [options] COMMIT-ISH [COMMIT-ISH]...")
+    .arg(
+      Arg::with_name("commit")
+        .required(true)
+        .index(1)
+        .multiple(true),
+    )
+    .get_matches();
+
+  let res = _main(matches.values_of("commit").unwrap().into_iter().collect());
+
+  if let Err(e) = res {
+    eprintln!("git error: {}", e);
+    std::process::exit(1);
+  }
+}
